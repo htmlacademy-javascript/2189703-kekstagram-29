0378a4033f2e496762233c523d16242091747b96
DianaPavlova <diana50888@gmail.com>
2023-06-13 08:34:29 +0200

    Исправляет тестирование функций.

--- js/functions.js
+++ js/functions.js
@@ -1,21 +1,11 @@
 // Функция для проверки длины строки.
-//let string;
-//let maxString;
-// Первый вариан с объявлением функции
+// Первый вариант объявления функции
 function checkStringLength (string, maxString) {
-  console.log('Проверяемая строка меньше максимально возможной ' + maxString + ' - ' + (string.length <= maxString));
+  return string.length <= maxString;
 }
 checkStringLength ('проверяемая строка', 20);
-checkStringLength ('проверяемая строка', 18);
-checkStringLength ('проверяемая строка', 10);
-
-// Второй вариант с анонимной (стрелочной) функцией и объявлением третьей переменной.
-const testStringLength = (string, maxString) => {
-  console.log('Проверяемая строка меньше максимально возможной ' + maxString + ' - ' + (string.length <= maxString));
-};
-testStringLength ('второй вариант проверки', 40);
-testStringLength ('второй вариант проверки', 30);
-testStringLength ('второй вариант проверки', 20);
+//let test1 = checkStringLength ('проверяемая строка', 20);
+//console.log('Проверяемая строка меньше максимально возможной ' + test1);
 
 // Функция для проверки, является ли строка палиндромом.
 function testPalindrome (line) {
@@ -25,15 +15,19 @@ function testPalindrome (line) {
   for (let i = readyLine.length - 1; i >= 0; i --){
     newLine = newLine += readyLine[i];
   }
-  console.log(line + ' - это палиндром - ' + (readyLine === newLine));
+  return (readyLine === newLine);
 }
-
-testPalindrome ('брАк');
-testPalindrome ('топот');
-testPalindrome ('ДовОд');
 testPalindrome ('Кекс');
-testPalindrome ('А роза упала на лапу Азора');
-testPalindrome ('Лёша на полке клопа нашёл ');
+/*Проверка палиндрома!
+line = 'топот';
+console.log(line + ' - это палиндром - ' + testPalindrome (line));
+line = 'А роза упала на лапу Азора';
+console.log(line + ' - это палиндром - ' + testPalindrome (line));
+
+console.log(testPalindrome ('ДовОд'));
+line = 'брАк';
+console.log(line + ' - это палиндром - ' + testPalindrome (line));*/
+
 
 // Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN:
 
@@ -47,14 +41,32 @@ function extractNumber(convertString){
   }
   advanceNumber = advanceNumber.replaceAll(' ', '');
   advanceNumber = parseInt(advanceNumber, 10);
-  console.log('Функция extractNumber из выражения "' + convertString + '" извлекает число: ' + advanceNumber);
+  return advanceNumber;
 }
+extractNumber('Проведено 8 проверок для 8 вариантов строки');
+//console.log(extractNumber('Проведено 8 проверок для 8 вариантов строки'));
+/* Проверка функции извлечения целого числа из строки
+convertString = '2023 год';
+console.log(extractNumber(convertString));
 
-extractNumber('2023 год');
-extractNumber('ECMAScript 2022');
-extractNumber('1 кефир, 0.5 батона');
-extractNumber('агент 007');
-extractNumber('а я томат');
-extractNumber(2023);
-extractNumber(-1);
-extractNumber(-1.5);
+convertString = 'ECMAScript 2022';
+console.log(extractNumber(convertString));
+
+convertString = '1 кефир, 0.5 батона';
+console.log(extractNumber(convertString));
+
+convertString = 'агент 007';
+console.log(extractNumber(convertString));
+
+convertString = 'а я томат';
+console.log(extractNumber(convertString));
+
+convertString = 2023;
+console.log(extractNumber(convertString));
+
+convertString = -1;
+console.log(extractNumber(convertString));
+
+convertString = -1.5;
+console.log(extractNumber(convertString));
+*/
