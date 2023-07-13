0e0e8b70fc56d59f65bbb889353977ed1ece62d0
DianaPavlova <diana50888@gmail.com>
2023-07-13 13:23:43 +0200

    Добавляет валидацию хештегов

--- js/form.js
+++ js/form.js
@@ -1,8 +1,11 @@
 import {isEscapeKey} from './util.js';
 
+const VALID_HASHTAG = /^#[a-zа-яё0-9]{1,19}$/i;
+const MAX_HACHTAG = 5;
+
 const form = document.querySelector('.img-upload__form');
-const modalForm = document.querySelector('.img-upload__overlay');
-const choicePhoto = document.querySelector('.img-upload__input');
+const modalForm = form.querySelector('.img-upload__overlay');
+const choicePhoto = form.querySelector('.img-upload__input');
 const backScreen = document.querySelector('body');
 const buttonCloseForm = form.querySelector('.img-upload__cancel');
 const hashtagsUser = form.querySelector('.text__hashtags');
@@ -14,14 +17,28 @@ const pristine = new Pristine(form, {
   errorTextClass: 'img-upload__field-wrapper-error-wrapper',
 });
 
-form.addEventListener('submit', (evt) => {
-  evt.preventDefault();
 
+const hashtagsUserArray = (string) => string.trim().split(' ').filter((array) => Boolean(array.length));
+
+const hashtagsUserValidSum = (value) => hashtagsUserArray(value).length <= MAX_HACHTAG;
+const hashtagsUserValidHashtag = (value) => hashtagsUserArray(value).every((string) => VALID_HASHTAG.test(string));
+const hashtagsUserValidUnique = (value) => {
+  const hashtagsUserArrayLover = hashtagsUserArray(value).map((string) => string.toLowerCase());
+  return hashtagsUserArrayLover.length === new Set(hashtagsUserArrayLover).size;
+};
+
+const errorTextValidSum = () => `Нельзя вводить больше ${ MAX_HACHTAG } хештегов!`;
+const errorTextValidHashtag = () => 'Хештег введён некорректно!';
+const errorTextValidUnique = () => 'Недопустимо вводить одинаковые хештеги!';
+
+pristine.addValidator(hashtagsUser, hashtagsUserValidSum, errorTextValidSum, 3);
+pristine.addValidator(hashtagsUser, hashtagsUserValidHashtag, errorTextValidHashtag, 2);
+pristine.addValidator(hashtagsUser, hashtagsUserValidUnique, errorTextValidUnique, 1);
+
+form.addEventListener('submit', (evt) => {
   const isValid = pristine.validate();
-  if (isValid) {
- //   console.log('OK');
-  } else {
- //   console.log('Non!!!');
+  if (!isValid) {
+    evt.preventDefault();
   }
 });
 
@@ -71,5 +88,3 @@ function closeForm () {
 
   document.removeEventListener('keydown', onDocumentKeydown);
 }
-
-
